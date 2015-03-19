f9744ef892e964f4575e71e8f96c28c2b04161fd
kevinMacPro <cke8671@hotmail.com>
2015-03-19 09:49:06 +0900

    extFind.py executable file(linker?) added

--- extFind.py
+++ extFind.py
@@ -1,37 +1,5 @@
 #!/ccnc_bin/venv/bin/python
-
-import os
-import re
-import argparse
-import textwrap
-import pandas as pd
-
-
-def extSearch(ext,location=os.getcwd()):
-    out = []
-    for root,dirs,files in os.walk(location):
-        for sFile in files:
-            name = sFile.split('.')[0]
-            extension = '.'.join(sFile.split('.')[1:])
-            if re.search(ext,extension,re.IGNORECASE):
-                out.append(os.path.join(root,sFile))
-    return out
-
-def countExt(ext,location=os.getcwd()):
-    extLocList = extSearch(ext,location)
-    rootC = [os.path.split(x)[0] for x in extLocList]
-    rootU = set(rootC)
-    countD = {}
-    for root in rootU:
-        count = len([x for x in extLocList if root in x])
-        countD[root] = count
-    return countD
-    
-def dict2pd(D):
-    return pd.DataFrame.from_dict(D,orient='index')
-
-def list2pd(D):
-    return pd.DataFrame.from_list(D,orient='index')
+from ccncpy import *
 
 def main(args):
     if not args.count:
