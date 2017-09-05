840f6c8b263bcb5c2249d9565e5f323bd11e4b9c
kcho <sky8671@gmail.com>
2017-09-05 09:33:59 +0900

    FS server check script

---
+++ fsServerCheck.py
@@ -0,0 +1,24 @@
+import re
+import sys
+from os.path import join
+import os
+import pandas as pd
+
+def main(dirLoc):
+    subjectList = [x for x in os.listdir(dirLoc) if re.search('(FEP)|(CHR)|(NOR)', x)]
+
+    serverList = pd.DataFrame()
+    for subject in subjectList:
+        fsLog = join(dirLoc, subject, 'scripts/recon-all.log')
+        with open(fsLog, 'r') as f:
+            server = f.readlines()[9].split(' ')[0:2]
+            subj_dict = {subject:{'server':' '.join(server)}}
+            serverList = pd.concat([serverList, pd.DataFrame.from_dict(subj_dict, orient='index')])
+
+    serverList = serverList.reset_index()
+    serverList.columns = ['subject', 'server']
+    serverList['group'] = serverList['subject'].str[:3]
+    print serverList.reset_index().groupby(['group', 'server']).count()
+
+if __name__=='__main__':
+    main(sys.argv[1])
